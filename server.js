f3167667da6dad8e795fa3f5aa485df9e5242700
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-07-01 17:22:44 +0500

    changed port server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}))
 
 
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log("Server is running", port);
 })
