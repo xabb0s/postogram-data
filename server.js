6b2c0275c0ecc660c37d33560fb841fc38d7c973
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-07-01 17:15:47 +0500

    project created

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log("Server is running", port);
+})
