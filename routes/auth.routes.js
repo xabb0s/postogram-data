9eb8c7d9210c76e1e4a1bab9803d72a23381f259
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-07-04 18:29:59 +0500

    added login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -29,8 +29,30 @@ router.post("/register", async (req, res) => {
     }
 })
 
-router.post("/login", (req, res) => {
-    res.send("Login")
+router.post("/login", async (req, res) => {
+
+    const { username, password } = req.body
+
+    const existUser = await findUser(username)
+    if (!existUser) {
+        res.status(400).json({
+            message: `User with username: ${username} not found.`
+        })
+    }
+    else if (existUser.password !== password) {
+        res.status(400).json({
+            message: `Wrong username or password.`
+        })
+    }
+    else {
+        res.status(200).json({
+            message: `Successful login.`,
+            user: {
+                username: existUser.username,
+                token: existUser.token
+            }
+        })
+    }
 })
 
 
