9675104891ab90daeff42994ab84162bf042b906
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-07-13 18:02:31 +0500

    added comment

---
+++ routes/comments.routes.js
@@ -0,0 +1,71 @@
+const { Router } = require("express")
+const { addComment, findCommentsByAuthor, findCommentsByPost } = require("../services/comments.service.js")
+const authentification = require("../middlewares/authentification.js")
+
+const router = Router()
+
+router.post("/comments", authentification, async (req, res) => {
+  const { message, postId} = req.body
+  const authorId = res.locals.user.id
+
+  try {
+    const comment = addComment(message, postId, authorId)
+
+    res.json({
+      status: "ok",
+      msg: "Comment posted.",
+      comment: comment
+    })
+  }
+  catch (err) {
+    res.json({
+      status: "bad",
+      msg: "Error on post comment",
+      error: err
+    })
+  }
+})
+
+router.get("/comments/post/:id", authentification, async (req, res) => {
+  const postId = req.params.id
+
+  try {
+    const comments = findCommentsByPost(postId)
+
+    res.json({
+      status: "ok",
+      msg: "Everything is okay!",
+      comments: comments
+    })
+  }
+  catch (err) {
+    res.json({
+      status: "bad",
+      msg: "Error on find comments by Post",
+      error: err
+    })
+  }
+})
+
+router.get("/comments/author/:id", authentification, async (req, res) => {
+  const authorId = req.params.id
+
+  try {
+    const comments = findCommentsByAuthor(authorId)
+
+    res.json({
+      status: "ok",
+      msg: "Everything is okay",
+      comments: comments
+    })
+  }
+  catch (err) {
+    res.json({
+      status: "bad",
+      msg: "Error on find comments by Author",
+      error: err
+    })
+  }
+})
+
+router.get("/comments", authentification)
--- services/comments.service.js
+++ services/comments.service.js
@@ -1,3 +1,46 @@
-const prisma = require("../config/database.js")
+const prisma = require("../config/database.js");
 
-function addComment( message, postId )
+function addComment(message, postId, authorId) {
+  return prisma.comment.create({
+    data: {
+      message,
+      author: {
+        connect: {
+          id: authorId,
+        },
+      },
+      post: {
+        connect: {
+          id: postId,
+        },
+      },
+    },
+  });
+}
+  
+function findCommentsByPost(postId) {
+  return prisma.comment.findMany({
+    where: {
+      postId: postId,
+    },
+  });
+}
+
+function findCommentsByAuthor(authorId) {
+  return prisma.comment.findMany({
+    where: {
+      authorId: authorId,
+    },
+  });
+}
+
+// function removeComment(id) {
+
+// }
+
+module.exports = {
+  addComment,
+  findCommentsByPost,
+  findCommentsByAuthor,
+  removeComment,
+};
